d58ae0da9ab4c50e67983be236fe54fdb5dee96a
saima2208 <saimasonya22@gmail.com>
2025-02-19 22:27:16 +0600

    update

---
+++ src/app/product-information/product-information.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-information',
+  imports: [],
+  templateUrl: './product-information.component.html',
+  styleUrl: './product-information.component.css'
+})
+export class ProductInformationComponent {
+
+}
